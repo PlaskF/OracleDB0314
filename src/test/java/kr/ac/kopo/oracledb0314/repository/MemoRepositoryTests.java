2af17969a3e0099cb5511666c8382dea658ab871
PaksF <flaksfjtm@gmail.com>
2024-03-14 11:48:10 +0900

    2024-03-14

---
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.oracledb0314.repository;
+
+import kr.ac.kopo.oracledb0314.entity.Memo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass(){
+        System.out.println(memoRepository.getClass());
+    }
+
+    @Test
+    public void testInsertDummies(){
+        IntStream.rangeClosed(1,100).forEach(i ->{
+            Memo memo = kr.ac.kopo.oracledb0314.entity.Memo.builder().memoText("Dummy Data Test").build();
+            memoRepository.save(memo);
+        });
+    }
+}
