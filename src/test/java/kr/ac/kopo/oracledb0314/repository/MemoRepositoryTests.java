dbfbda14f2c6fedb374e20969fdb5ece9baca541
PaksF <flaksfjtm@gmail.com>
2024-03-21 11:18:39 +0900

    testSelect2(), testUpdate(), testDelete()

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -4,6 +4,7 @@ import kr.ac.kopo.oracledb0314.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -42,5 +43,35 @@ public class MemoRepositoryTests {
         }
     }
 
+    @Transactional
+    @Test
+    public void testSelect2(){
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100l;
 
+        Memo memo = memoRepository.getOne(mno); //조회 작업
+
+        System.out.println("==================================");
+
+        System.out.println(memo);
+
+    }
+
+    @Test
+    public void testUpdate(){
+
+        Memo memo = Memo.builder().mno(100l).memoText("Update Dummy Data 95").build();
+
+        Memo memo1 = memoRepository.save(memo);
+
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testDelete(){
+
+        long mno = 100l;
+
+        memoRepository.deleteById(mno);
+    }
 }
